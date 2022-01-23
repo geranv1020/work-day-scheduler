bca4ed07c1d8f80c3c3c4e165dc7d38250f1934d
Legolas <geran.vanatta@gmail.com>
2022-01-23 16:23:15 -0600

    saveBtn function, get item from local storage, current day function

--- develop/script.js
+++ develop/script.js
@@ -1,11 +1,27 @@
-const moment = require("moment");
+// current date at top of the page 
 
-moment().format("dddd, MMMM Do YYYY");
+$(document).ready(function () {
+    $("#currentDay").text("Today is " + moment().format("dddd, MMMM Do YYYY"));
+});
 
+// save schedule info
+$(document).ready(function () {
+    $(.saveBtn).on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+})
 // apply new class if the hour has passed
-if (moment().isAfter(time)) {
-    $(taskEl).addClass("past");
-}
-else if (Math.abs(moment().diff(time, "hours")) <=1) {
-    $(taskEl).addClass("past");
-}
+
+
+$("#09 .description").val(localStorage.getItem("#09"));
+$("#10 .description").val(localStorage.getItem("#10"));
+$("#11 .description").val(localStorage.getItem("#11"));
+$("#12 .description").val(localStorage.getItem("#12"));
+$("#13 .description").val(localStorage.getItem("#13"));
+$("#14 .description").val(localStorage.getItem("#14"));
+$("#15 .description").val(localStorage.getItem("#15"));
+$("#16 .description").val(localStorage.getItem("#16"));
+$("#17 .description").val(localStorage.getItem("#17"));
