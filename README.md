fcb1dfdd1c38eb57b0c1b8687e0fb57710436487
Legolas <geran.vanatta@gmail.com>
2022-01-23 19:09:32 -0600

    changed classes of text area based on time, added readme, finished

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# work-day-scheduler
+# Work-Day-Scheduler
+
+## Purpose
+Daily planner where you can schedule things by the hour. Type your plans in and click the blue save button. The text areas will change color based on the time of day.
+
+## Built with
+*HTML
+*CSS
+*JavaScript
+
+## Link
+https://geranv1020.github.io/work-day-scheduler/
+
+## Screenshot
+![Image](./develop/images/scheduler.png)
+
+## Contribution
+Made by Geran Vanatta
--- develop/script.js
+++ develop/script.js
@@ -1,27 +1,59 @@
-// current date at top of the page 
 
+// current date at top of the page 
 $(document).ready(function () {
     $("#currentDay").text("Today is " + moment().format("dddd, MMMM Do YYYY"));
 });
 
 // save schedule info
 $(document).ready(function () {
-    $(.saveBtn).on("click", function () {
+    // saveBtn click listener
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
+        //save in localStorage
         localStorage.setItem(time, text);
     })
-})
-// apply new class if the hour has passed
+
+    function timeChecker () {
+        var timePresent = moment().hour();
+
+        // keep track of time change
+        $("textarea").each(function () {
+            var textTime = parseInt($(this).attr("id"));
+                
+            // if current time is past the schedule hour, change class
+            if (textTime < timePresent) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            // if current time is equal to the schedule hour, change class
+            else if (textTime === timePresent) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            // if current time is before the schedule hour, change class
+            else if (textTime > timePresent) {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
 
 
-$("#09 .description").val(localStorage.getItem("#09"));
-$("#10 .description").val(localStorage.getItem("#10"));
-$("#11 .description").val(localStorage.getItem("#11"));
-$("#12 .description").val(localStorage.getItem("#12"));
-$("#13 .description").val(localStorage.getItem("#13"));
-$("#14 .description").val(localStorage.getItem("#14"));
-$("#15 .description").val(localStorage.getItem("#15"));
-$("#16 .description").val(localStorage.getItem("#16"));
-$("#17 .description").val(localStorage.getItem("#17"));
+    // Get item from local storage
+    $("#09 .description").val(localStorage.getItem("#09"));
+    $("#10 .description").val(localStorage.getItem("#10"));
+    $("#11 .description").val(localStorage.getItem("#11"));
+    $("#12 .description").val(localStorage.getItem("#12"));
+    $("#13 .description").val(localStorage.getItem("#13"));
+    $("#14 .description").val(localStorage.getItem("#14"));
+    $("#15 .description").val(localStorage.getItem("#15"));
+    $("#16 .description").val(localStorage.getItem("#16"));
+    $("#17 .description").val(localStorage.getItem("#17"));
+
+    timeChecker();
+});
